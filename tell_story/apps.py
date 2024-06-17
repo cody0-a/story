09cb2f2015b910384a537a9007fa403a99edbaed
cody0-a <isaabe83@gmail.com>
2024-06-18 01:25:26 +1300

    Ignore virtual environment directory

---
+++ tell_story/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TellStoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tell_story'
---
+++ tell_story/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'tell_story'
+urlpatterns = [
+    path('', views.story, name='story'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('room/<int:room_id>', views.room, name='room'),
+    path('stories/', views.stories, name='stories'),
+    path('story/<int:story_id>', views.story, name='story'),
+    path('add-story/', views.add_story, name='add_story'),
+    path('edit-story/<int:story_id>', views.edit_story, name='edit_story'),
+    path('delete-story/<int:story_id>', views.delete_story, name='delete_story'),
+    path('add-comment/<int:story_id>', views.add_comment, name='add_comment'),
+]
